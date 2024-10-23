f5e231937316c2c618fe40a93fd430a31b0a38f2
Gktwo <1209951416@qq.com>
2024-10-23 23:53:21 +0800

    .net core 6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,34 +4,25 @@ using System.Runtime.InteropServices;
 using MelonLoader;
 using DmmCheat;
 
+// 在此类的 SDK 样式项目中，现在，在此文件中早前定义的几个程序集属性将在生成期间自动添加，并使用在项目属性中定义的值进行填充。有关包含的属性以及如何定制此过程的详细信息，请参阅
+// https://aka.ms/assembly-info-properties
 [assembly: MelonGame(null, null)]
 [assembly: MelonInfo(typeof(Loader), "DmmCheat", "0.0.1", "gktwo", null)]
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("DmmCheatMod")]
+
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DmmCheatMod")]
+
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// 将 ComVisible 设置为 false 会使此程序集中的类型
-//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
-//请将此类型的 ComVisible 特性设置为 true。
+// 将 ComVisible 设置为 false 会使此程序集中的类型对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，请将该类型的 ComVisible
+// 属性设置为 true。
+
 [assembly: ComVisible(false)]
 
-// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("fa8b9f72-1451-4320-b8a0-7f7d972442d7")]
+// 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID。
 
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("2498cc8d-2458-4a84-8f25-5200fd142b75")]
